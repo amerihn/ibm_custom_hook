0a390e97db974f0aacd82347fd15fb1c684e382f
amerihn <amerihn.art@gmail.com>
2024-07-09 09:52:20 +0700

    fetch foods and yoga data

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
+
 const UseFetch = (url) => {
-    
+   const[data,setData]=useState();
+   useEffect(() =>{
+      fetch(url) //This initiates an HTTP request to the specified url.
+      .then((res) => res.json()) //This converts the response from the server to JSON format.
+      .then((data) => setData(data)) //This sets the retrieved data to the state variable data.
+   },
+   []);
+   return [data];
 }
 
 export default UseFetch
